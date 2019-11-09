eaf99e7ffd1bfddca4955215c24b70eb2987f05a
alvin0319 <alvin7810@naver.com>
2019-11-09 10:58:11 +0900

    random hash

--- src/Towny/util/Util.php
+++ src/Towny/util/Util.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace Towny\util;
 
 use pocketmine\Server;
+use pocketmine\utils\UUID;
 use pocketmine\world\Position;
 
 class Util{
@@ -14,5 +15,9 @@ class Util{
 	public static function hash2pos(string $hash) : Position{
 		[$x, $y, $z, $world] = explode(":", $hash);
 		return new Position((float)$x, (float)$y, (float)$z, Server::getInstance()->getWorldManager()->getWorldByName($world));
+	}
+
+	public static function generateRandomHash() : string{
+		return UUID::fromRandom()->toString();
 	}
 }
